6c1767a358a0bf032abf14dea92676359af9048d
mansigupta121 <mansi.gupta042004@gmail.com>
2026-03-01 22:20:38 +0530

    Updated config paths for Colab split directory

--- src/config.py
+++ src/config.py
@@ -1,20 +1,12 @@
 import os
 
-# =========================
-# PATHS
-# =========================
+BASE_DIR = "/content/drive/MyDrive/SmartWardrobe/body_shape_recommendor/data"
 
-BASE_DIR = "/content/drive/MyDrive/SmartWardrobe/body_shape_recommender/data"
-
-TRAIN_DIR = os.path.join(BASE_DIR, "train")
-VAL_DIR = os.path.join(BASE_DIR, "val")
-TEST_DIR = os.path.join(BASE_DIR, "test")
+TRAIN_DIR = os.path.join(BASE_DIR, "split/train")
+VAL_DIR   = os.path.join(BASE_DIR, "split/val")
+TEST_DIR  = os.path.join(BASE_DIR, "split/test")
 
 BODY_VECTOR_CSV = os.path.join(BASE_DIR, "body_vectors.csv")
-
-# =========================
-# TRAINING PARAMS
-# =========================
 
 IMAGE_SIZE = 224
 BATCH_SIZE = 16
